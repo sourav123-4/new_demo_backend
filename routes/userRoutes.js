ec156819ffe3217d1a1b749188fb355a9e5b212d
sourav <souravmahanty12@gmail.com>
2024-11-06 23:54:50 +0530

    initial commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+// routes/userRoutes.js
+const express = require('express');
+const { createUser, getUsers } = require('../controllers/userController');
+
+const router = express.Router();
+
+router.post('/users', createUser);
+router.get('/users', getUsers);
+
+module.exports = router;
