c4ede7bf42c1668c4453b53f08e16c32e5326ee9
sourav <souravmahanty12@gmail.com>
2024-12-16 23:42:45 +0530

    added index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const express = require('express');
-const app = express();
-const PORT = 4000;
-
-app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
-});
+const app = require('./src/App');
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
-module.exports = app;
